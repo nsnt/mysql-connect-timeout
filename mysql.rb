af63920f53abc7cc7ef098f9aa3d2242e0594619
TANIGUCHI, Noburou <taniguchi.noburou@po.ntts.co.jp>
2013-10-10 15:34:07 +0900

    Add begin-rescue

--- mysql.rb
+++ mysql.rb
@@ -4,8 +4,13 @@
 require "mysql2"
 
 client = nil
-5.times do
-  client = Mysql2::Client.new(:host => "192.168.14.111", :username => "root", :password => "mysql", :connect_timeout => 1)
+5.times do |time|
+  begin
+    client = Mysql2::Client.new(:host => "192.168.14.111", :username => "root", :password => "mysql", :connect_timeout => 1)
+    break if client
+  rescue Mysql2::Error => err_mysql2
+    puts "#{err_mysql2.message}: #{time + 1} times"
+  end
 end
 
 if client
