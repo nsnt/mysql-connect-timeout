5d4ce3871b8ac1afd7d9f52e38ac29a7d9f74a90
TANIGUCHI, Noburou <taniguchi.noburou@po.ntts.co.jp>
2013-10-10 21:27:32 +0900

    Introduce retry_mysql to simulate operation in CloudN RDB Gateway

--- mysql.rb
+++ mysql.rb
@@ -21,14 +21,21 @@ params = {
 params[:connect_timeout] = connect_timeout if connect_timeout
 params[:read_timeout] = read_timeout if read_timeout
 
-client = nil
-5.times do |time|
-  begin
-    client = Mysql2::Client.new(params)
-    break if client
-  rescue Mysql2::Error => err_mysql2
-    puts "#{err_mysql2.message}: #{time + 1} times"
+def mysql_retry(num, &blk)
+  result = nil
+  num.times do |time|
+    begin
+      result = blk.call
+      break if result
+    rescue Mysql2::Error => err_mysql2
+      puts "#{err_mysql2.message}: #{time + 1} times"
+    end
   end
+  result
+end
+
+client = mysql_retry(3) do
+  Mysql2::Client.new(params)
 end
 
 if client
@@ -38,45 +45,76 @@ else
   exit 0
 end
 
+result = mysql_retry(3) do
+  client.query("show databases;")
+end
+
 target_db = "testtesttest"
 target_db_exists = false
-begin
-  result = client.query("show databases;")
-  puts "Show database: OK"
+if result
+  puts "Show databases: OK"
   result.each do |row|
     puts "#{row.inspect}"
     target_db_exists = true if row.values.include?(target_db)
   end
-rescue => err
-  puts "Show database: NG"
-  puts err.message
+else
+  puts "Show databases: NG"
   exit 0
 end
 
-begin
-  result = client.query("show tables in mysql;")
+result = mysql_retry(3) do
+  client.query("show tables in mysql;")
+end
+
+if result
   puts "Show tables: OK"
   result.each do |row|
     puts "#{row.inspect}"
   end
-rescue => err
+else
   puts "Show tables: NG"
-  puts err.message
   exit 0
 end
 
-begin
+result = mysql_retry(3) do
+  val = false
   if target_db_exists
-    result = client.query("drop database #{target_db};")
-    puts "Drop database: OK"
+    client.query("drop database #{target_db};")
+    val = true
+  else
+    val = nil
   end
-  result = client.query("create database #{target_db};")
-  puts "Create database: OK"
-  result = client.query("drop database #{target_db};")
+  val
+end
+
+if result
   puts "Drop database: OK"
-rescue => err
-  puts "Create/Drop database: NG"
-  puts err.message
-  puts err.backtrace
+elsif result.nil?
+  puts "Need not to drop database '#{target_db}'"
+else
+  puts "Drop database: NG"
+end
+
+result = mysql_retry(3) do
+  client.query("create database #{target_db};")
+  true
+end
+
+if result
+  puts "Create database '#{target_db}': OK"
+else
+  puts "Create database '#{target_db}': NG"
+  exit 0
+end
+
+result = mysql_retry(3) do
+  client.query("drop database #{target_db};")
+  true
+end
+
+if result
+  puts "Drop database '#{target_db}': OK"
+else
+  puts "Drop database '#{target_db}': NG"
   exit 0
 end
