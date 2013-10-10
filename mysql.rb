d86564ad45451b76ebc27946a73e162540858575
TANIGUCHI, Noburou <taniguchi.noburou@po.ntts.co.jp>
2013-10-10 15:46:05 +0900

    Introduce optparse

--- mysql.rb
+++ mysql.rb
@@ -2,11 +2,27 @@
 # encoding: utf-8
 
 require "mysql2"
+require "optparse"
+
+host = username = password = connect_timeout = nil
+opt = OptionParser.new
+opt.on('-H VAL') {|val| host = val}
+opt.on('-U VAL') {|val| username = val}
+opt.on('-P VAL') {|val| password = val}
+opt.on('-T VAL') {|val| connect_timeout = val.to_i}
+opt.parse!(ARGV)
+
+params = {
+  :host => (host || "192.168.14.111"),
+  :username => (username || "root"),
+  :password => (password || "mysql"),
+  :connect_timeout => (connect_timeout || 1)
+}
 
 client = nil
 5.times do |time|
   begin
-    client = Mysql2::Client.new(:host => "192.168.14.111", :username => "root", :password => "mysql", :connect_timeout => 1)
+    client = Mysql2::Client.new(params)
     break if client
   rescue Mysql2::Error => err_mysql2
     puts "#{err_mysql2.message}: #{time + 1} times"
